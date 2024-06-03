dec2e7e326ba54d48da9f8a5a6e39639fadbfd9c
SEF-Assessment4 <sheen2906@gmail.com>
2024-06-03 13:30:17 +1000

    api chaange

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'ec2-3-106-176-252.ap-southeast-2.compute.amazonaws.com',
+    baseURL: 'https://ec2-3-106-176-252.ap-southeast-2.compute.amazonaws.com',
 });
 
 export default api;
