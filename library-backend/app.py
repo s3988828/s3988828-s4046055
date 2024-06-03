d14558952328cb231133275a8fc7819b13864f63
SEF-Assessment4 <sheen2906@gmail.com>
2024-06-03 15:22:03 +1000

    chnages made

--- library-backend/app.py
+++ library-backend/app.py
@@ -14,7 +14,7 @@ from botocore.config import Config
 
 
 app = Flask(__name__)
-CORS(app, resources={r"*": {"origins": "https://ec2-3-106-176-252.ap-southeast-2.compute.amazonaws.com"}})
+CORS(app, resources={r"/api/*": {"origins": "https://ec2-3-106-176-252.ap-southeast-2.compute.amazonaws.com"}})
 #CORS(app, resources={r"/api/*": {"origins": "https://ec2-3-27-159-85.ap-southeast-2.compute.amazonaws.com"}})
 app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Ensure this is securely generated and consistent
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
@@ -42,7 +42,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/register', methods=['POST'])
+@app.route('/api/register', methods=['POST'])
 def register():
     data = request.json
     username = data['username']
